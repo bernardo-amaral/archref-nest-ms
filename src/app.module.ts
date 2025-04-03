138abe7561b5b03851390c32e640076b4f669855
Bernardo Amaral <bernardo-amaral@users.noreply.github.com>
2025-04-03 13:15:36 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { RabbitMQService } from './rabbitmq.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'RMQ_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://guest:guest@localhost:5672/local'],
-          queue: 'api-queue',
-          queueOptions: {
-            durable: true,
-            deadLetterExchange: 'api-queue-dlx',
-            exchange: {
-              name: 'api-queue-exchange',
-              type: 'direct',
-              echangeOpts: {
-                durable: true,
-              },
-            },
-          },
-        },
-      },
-    ]),
-  ],
-  controllers: [AppController],
-  providers: [AppService, RabbitMQService],
-  exports: [RabbitMQService],
+  imports: [RabbitMqModule],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
