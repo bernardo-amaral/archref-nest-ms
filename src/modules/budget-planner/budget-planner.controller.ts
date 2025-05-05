39fbf99070a1c89d6178b8593121799a638ecd85
Bernardo Amaral <dev02@rksoftware.com.br>
2025-05-05 15:26:53 -0300

    Fix docker

--- src/modules/budget-planner/budget-planner.controller.ts
+++ src/modules/budget-planner/budget-planner.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('budget-planner')
-export class BudgetPlannerController {}
+export class BudgetPlannerController {
+  @Post()
+  create(@Body() createCatDto: any, @Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
+  }
+
+  @Get()
+  findAll(@Res() res: Response) {
+    res.status(HttpStatus.OK).json([]);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: any) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
